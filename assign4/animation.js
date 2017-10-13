c40cb2f52ede5a7bcbe194d16529f025d950066c
ckchessmaster <Genesis1:1>
2017-10-13 18:24:56 -0400

    Created battery drain animation

--- assign4/animation.js
+++ assign4/animation.js
@@ -5,6 +5,8 @@ var light = new Image();
 var lightUnlit = new Image();
 var lightSwitch = new Image();
 var isOn = false;
+var currentFrame = 0;
+var batteryCharge = 0; // 0 is the highest charge level 100 is lowest
 
 function init() {
   canvas = $('#mainCanvas')[0];
@@ -34,9 +36,9 @@ function drawScreen() {
   context.clearRect(0, 0, 1000, 600);
 
   // draw the images
-  context.drawImage(battery, 0, 190, 200,200);
 
-  if(isOn == true) {
+
+  if(isOn == true && batteryCharge < 100) {
     context.drawImage(light, 750, 195, 200, 200);
   } else {
     context.drawImage(lightUnlit, 750, 195, 200, 200);
@@ -51,29 +53,72 @@ function drawScreen() {
 
 
   // draw the wires
-  drawLine(100, 230, 100, 140, 'red');
-  drawLine(100, 145, 400, 145, 'red');
+  drawLine(100, 230, 100, 140, 'red'); // segment 1
+  drawLine(100, 145, 400, 145, 'red'); // segment 2
 
-  if(isOn == true) {
+  if(isOn == true && batteryCharge < 100) {
     color = 'red';
   } else {
     color = 'white';
   }
 
-  drawLine(490, 145, 740, 145, color);
-  drawLine(740, 140, 740, 280, color);
-  drawLine(740, 275, 760, 275, color);
-  drawLine(760, 309, 740, 309, color);
-  drawLine(740, 304, 740, 440, color);
-  drawLine(100, 435, 740, 435, color);
-  drawLine(100, 350, 100, 440, color);
+  drawLine(490, 145, 740, 145, color); // segment 3
+  drawLine(740, 140, 740, 280, color); // segment 4
+  drawLine(740, 275, 760, 275, color); // segment 5
+  drawLine(760, 309, 740, 309, color); // segment 6
+  drawLine(740, 304, 740, 440, color); // segment 7
+  drawLine(100, 435, 740, 435, color); // segment 8
+  drawLine(100, 350, 100, 440, color); // segment 9
+  var offset = 0;
 
+  /* draw the energy flow */
+  // segment 1
+  for(i=245 + offset; i>100; i-=50) {
+    context.beginPath();
+    context.arc(100, i, 10, 0, 2*Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+    context.closePath();
+  }
+
+  //context.beginPath();
+  //context.fillRect(75, 250, 50, 90);
+  //context.fillStyle = 'black';
+  //context.closePath();
+
+  /* draw the battery */
+  // the background
+  context.fillStyle = 'black';
+  context.fillRect(75, 245, 50, 100);
+
+  // current charge
+  if((currentFrame % 6) == 0) {
+    if(isOn == true && batteryCharge < 100) {
+      batteryCharge+=1;
+    }
+  }//end frame check
+  context.fillStyle = 'green';
+  context.fillRect(75, 245 + batteryCharge, 50, 100 - batteryCharge); // starts at 245/100
+
+  // fix part of the outline
+  //context.filleStyle = 'white';
+  //context.fillRect(75, 200, 10, 10);
+
+  // the outline
+  context.drawImage(battery, 0, 190, 200,200);
+
+  // go to the next frame
+  if(currentFrame >= 60) {
+    currentFrame = 0;
+  } else {
+    currentFrame++;
+  }//end if/else
   window.requestAnimationFrame(drawScreen);
-}
+}//end drawScreen
 
 function onMainSwitchClick() {
   isOn = !isOn;
-}
+}//end onMainSwitchClick
 
 function drawLine(startX, startY, endX, endY, color = 'white') {
   // set to default color if nothing is specified
